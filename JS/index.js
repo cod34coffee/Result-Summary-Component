12be9c693b4beeadc3e84c247d83954c20e18c95
cod34coffee <87444893+cod34coffee@users.noreply.github.com>
2023-03-18 20:37:51 -0500

    add
    Adding more to it.

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+
+// There are 4 items inside the array, each item is an object with 3 properties. A category, score, icon.
+
+const answersArray = [
+      {
+        id: 0,
+        category: "Reaction",
+        score: 80,
+        icon: "./assets/images/icon-reaction.svg"
+      },
+      {
+        id: 1,
+        category: "Memory",
+        score: 92,
+        icon: "./assets/images/icon-memory.svg"
+      },
+      {
+        id: 2,
+        category: "Verbal",
+        score: 61,
+        icon: "./assets/images/icon-verbal.svg"
+      },
+      {
+        id: 3,
+        category: "Visual",
+        score: 72,
+        icon: "./assets/images/icon-visual.svg"
+      }
+];
+
+let htmlCode = ``;
+
+answersArray.
